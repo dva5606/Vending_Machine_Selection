d9eb6900b40bf5f63d1b14e149bb34bd4f461a6e
dva5606 <86691169+dva5606@users.noreply.github.com>
2021-10-28 09:21:07 +1300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,29 @@
+enum RadioMessage {
+    message1 = 49434
+}
 input.onButtonPressed(Button.A, function () {
-    let PEACH = 0
-    radio.sendString("" + (PEACH))
+    radio.sendString("PEACH")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "PEACH") {
+        if (PEACH == true) {
+            PEACH = false
+        } else if (PEACH == false) {
+            PEACH = true
+        }
+    }
+    if (receivedString == "MANGO") {
+        if (MANGO == true) {
+            MANGO = false
+        } else if (MANGO == false) {
+            MANGO = true
+        }
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    let MANGO = 0
-    radio.sendString("" + (MANGO))
+    radio.sendString("MANGO")
 })
+let MANGO = false
+let PEACH = false
 basic.showIcon(IconNames.Duck)
 radio.setGroup(69)
